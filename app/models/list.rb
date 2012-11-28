a9e29e18e4616311fcb2877791d592215d9142ee
joseph Borremans <joseph.borremans@gmail.com>
2012-11-28 15:52:24 +0100

    placed validations

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,11 @@
 class List < ActiveRecord::Base
   attr_accessible :completed, :idea, :todo
+
+
+# validation stuff
+
+validates :idea, :completed, :todo, :presence => true
+validates :idea, :todo, :uniqueness => true
+validates :idea, :todo, :length => {:minimum => 5}
+validates :idea, :todo, :length => {:maximum => 25}
 end
